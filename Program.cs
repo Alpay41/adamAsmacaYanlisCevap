2610875fef677cd661938bc8739d12f7265de0f6
AlpayCa <alpay41@gmail.com>
2023-05-04 18:13:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,265 @@
+﻿namespace adamAsmacaYanlisCevap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //int[] sayılar = { 5, 3, 6, 4, 7, 9, 2, 12 };
+            //int[] sıralıSayılar = new int[sayılar.Length];
+
+            //for (int i = 0; i < sayılar.Length; i++)
+            //{
+            //    if (sayılar[i] != i)
+            //    {
+            //        sıralıSayılar[i] = sayılar[i];
+
+            //        Console.Write(sıralıSayılar[i] + " ");
+
+            //    }
+            //}
+
+            //Decimal sayi = 7999999999999999999;
+            //Math.Cbrt(7999999999999999999);
+
+            //Console.WriteLine(Math.Cbrt(7999999999999999999)  );
+
+            //string başla = Console.CursorTop();
+
+
+
+
+            Daragaci();
+
+
+
+
+
+
+        }
+
+        public static void Daragaci()
+        {
+            string[] daragaci = // 0 yanlış cevap
+               {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < daragaci.Length; i++)
+            {
+                for (int j = 0; j < daragaci[i].Length; j++)
+                {
+                    Console.Write(daragaci[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam1()
+        {
+            string[] adam1 = // 1 yanlış cevap
+            {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "               X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < adam1.Length; i++)
+            {
+                for (int j = 0; j < adam1[i].Length; j++)
+                {
+                    Console.Write(adam1[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam2()
+        {
+            string[] adam2 = // 2 yanlış cevap
+            {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "               X	",
+                 "               X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < adam2.Length; i++)
+            {
+                for (int j = 0; j < adam2[i].Length; j++)
+                {
+                    Console.Write(adam2[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam3()
+        {
+            string[] adam3 = // 3 yanlış cevap
+               {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "     X         X	",
+                 "   X X         X	",
+                 " X   X         X	",
+                 "X    X         X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "               X	",
+                 "               X	",
+                 "              XXX"
+            };
+
+            for (int i = 0; i < adam3.Length; i++)
+            {
+                for (int j = 0; j < adam3[i].Length; j++)
+                {
+                    Console.Write(adam3[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam4()
+        {
+            string[] adam4 = // 4 yanlış cevap
+            {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 " X   X   X     X	",
+                 "X    X     X   X	",
+                 "     X         X	",
+                 "     X         X	",
+                 "               X	",
+                 "               X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < adam4.Length; i++)
+            {
+                for (int j = 0; j < adam4[i].Length; j++)
+                {
+                    Console.Write(adam4[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam5()
+        {
+            string[] adam5 = // 5 yanlış cevap
+              {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 " X   X   X     X	",
+                 "X    X     X   X	",
+                 "     X         X	",
+                 "   X X         X	",
+                 " X             X	",
+                 "X              X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < adam5.Length; i++)
+            {
+                for (int j = 0; j < adam5[i].Length; j++)
+                {
+                    Console.Write(adam5[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Adam6()
+        {
+            string[] adam6 = // 6 yanlış cevap
+               {
+                 "     X X X X X X 	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "  X     X      X	",
+                 "   X X X       X	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 " X   X   X     X	",
+                 "X    X     X   X	",
+                 "     X         X	",
+                 "   X X X       X	",
+                 " X       X     X	",
+                 "X          X   X	",
+                 "              XXX "
+            };
+
+            for (int i = 0; i < adam6.Length; i++)
+            {
+                for (int j = 0; j < adam6[i].Length; j++)
+                {
+                    Console.Write(adam6[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
